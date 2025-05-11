e22ddf8e1f966d39c4b658bd5c34ff2f4f4e74b9
Thuy Nguyen <thuy.ngnhu@gmail.com>
2025-05-11 23:33:05 +0200

    feat: update content

--- src/app/(app)/page-data.tsx
+++ src/app/(app)/page-data.tsx
@@ -34,7 +34,6 @@ const techNames = [
   "kubernetes",
   "git",
   "github/gray",
-  "gitlab",
   "jetbrains/gray",
   "figma",
 ];
@@ -43,7 +42,7 @@ export const features: BentoCardProps[] = [
   {
     Icon: LaughIcon,
     name: "Hi, I'm Thuy",
-    description: "Welcome to my corner of the internet. Thanks for stopping by!",
+    description: "Welcome to my corner of the internet. Thanks for stopping by! (And actually, it's Thụy)",
     className: "md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-2",
     href: "/",
     background: <img src="/me.png" className="saturate-400"/>,
@@ -59,7 +58,7 @@ export const features: BentoCardProps[] = [
   {
     Icon: DraftingCompassIcon,
     name: "Software Engineer",
-    description: "For the past 2.5 years, I've been a backend-focused software engineer in the banking world, where I also enjoy pitching in with frontend work and actively exploring DevOps. My goal is to help deliver reliable applications and keep growing my skills along the way!",
+    description: "For the past 2.5 years, I've been a backend-focused software engineer in the banking world, where I also enjoy pitching in with frontend work and actively exploring DevOps. My goal is to grow my skills and deliver reliable applications along the way!",
     href: "/",
     className: "md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-3",
     background: "https://img.freepik.com/free-photo/cute-little-dog-impersonating-business-person_23-2148985935.jpg?t=st=1746991008~exp=1746994608~hmac=463f24d39d5fa584a4a50478a7a57653fbe558c663728ecce42f91c608b34524&w=996",
@@ -93,7 +92,7 @@ export const features: BentoCardProps[] = [
     background: <div className="flex items-center justify-center">
       <IconCloud images={techNames.map(
         (techName) => `https://cdn.simpleicons.org/${techName}`,
-      )} className="w-full"/>
+      )} className="w-full z-10"/>
     </div>,
     className: "md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-4",
   },
@@ -111,8 +110,8 @@ export const features: BentoCardProps[] = [
   },
   {
     Icon: QuoteIcon,
-    name: "To the world, you may be one person, but to one person, you may be the world.",
-    description: "That's my favorite quote",
+    name: "\" To the world, you may be one person, but to one person, you may be the world.\"",
+    description: "That's my favorite quote!",
     href: "/",
     className: "md:col-start-1 md:col-end-3 md:row-start-6 md:row-end-7",
     background: <img
@@ -123,7 +122,7 @@ export const features: BentoCardProps[] = [
     Icon: ChefHatIcon,
     name: "Still cooking...",
     description:
-      "If I weren't a software engineer, I'd be a cook. I love everything about food – cooking it, eating it. And there's something really great about knowing my food makes people happy.",
+      "If I weren't a software engineer, I'd be a cook. I love everything about food, from cooking it to enjoying it. And it feels really great knowing that my cooking makes people happy.",
     href: "/",
     background: <img
       src="https://images.unsplash.com/photo-1634898635164-fbe5425b9d86?q=80&w=3297&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -134,7 +133,7 @@ export const features: BentoCardProps[] = [
     Icon: ShapesIcon,
     name: "Hobbies",
     description:
-      "Outside of the kitchen, I also enjoy exploring new places through travel and diving into the world of video games and board games.",
+      "Outside of the kitchen, I also enjoy traveling and exploring new places. I also love diving into the world of video games and board games.",
     href: "/",
     background: "https://img.freepik.com/free-photo/cute-beagle-dog-sitting-basket-while-wearing-goggles-pathway-nature-trees_181624-46010.jpg?t=st=1746990461~exp=1746994061~hmac=12f571670fa55bbbaf0b0ce7a1c2c4d37c4fd5d3589883eaea735ca531aac12d&w=740",
     className: "md:col-start-3 md:col-end-4 md:row-start-6 md:row-end-6",
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,12 +5,16 @@ import {features} from "@/app/(app)/page-data";
 export default function HomePage() {
   return (
     <div className="container-wrapper flex flex-1 flex-col">
-      <div className="container py-4 flex flex-col">
+      <div className="container py-4 flex flex-col items-center gap-8">
         <BentoGrid className="lg:grid-rows-3">
           {features.map((feature) => (
             <BentoCard key={feature.name} {...feature} />
           ))}
         </BentoGrid>
+        <div className="py-8 flex flex-col items-center gap-2 text-muted-foreground">
+          <h1>🐶</h1>
+          <h6 className="font-normal!">{"In case you haven't realized, I love dogs."}</h6>
+        </div>
       </div>
     </div>
   );
